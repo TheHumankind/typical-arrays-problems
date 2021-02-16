d826796d9a4ebacf41ebe502f4c9936e67606736
vlad <longsno@gmail.com>
2021-02-16 18:09:10 +0300

    Tests passed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  } 
+ 
+  let i = 0,
+      minVarible = array[i];
+  
+  for (i in array){
+    if (array[i] < minVarible) minVarible = array[i];
+  }
+
+  return minVarible;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  } 
+
+  let i = 0,
+      maxVarible = array[i];
+  
+  for (i in array){
+    if (array[i] > maxVarible) maxVarible = array[i];
+  }
+  return maxVarible;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  } 
+
+  let i = 0,
+      avgVarible = 0;
+
+  for(i in array) {
+    avgVarible = avgVarible + array[i];
+  }
+
+  return avgVarible / array.length;
 }
